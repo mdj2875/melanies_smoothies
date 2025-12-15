72f6d54038a517e7d6ab6e63aeb350350402471c
mdj2875 <mdj2875@gmail.com>
2025-12-15 14:29:17 -0500

    Update smoothie_app.py
    remove df

--- smoothie_app.py
+++ smoothie_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 
 ingredients_list = st.multiselect(
     "Choose up to five ingredients:",
