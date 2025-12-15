c6b3d09701061d3903b711794044eae76312e7b3
mdj2875 <mdj2875@gmail.com>
2025-12-15 10:37:28 -0500

    Update smoothie_app.py
    move to for block

--- smoothie_app.py
+++ smoothie_app.py
@@ -2,9 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -30,6 +27,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values 
 ('""" + ingredients_string + """','"""+name_on_order+"""')"""
