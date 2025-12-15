aa7e7fdf1647e2d6a9ef23aa576d26fbc431dae9
mdj2875 <mdj2875@gmail.com>
2025-12-15 10:34:38 -0500

    Update smoothie_app.py
    add sf_df

--- smoothie_app.py
+++ smoothie_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=st.text(smoothiefroot_response.json()),use_container_width=True)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
