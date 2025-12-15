4cd2191df22a55266830460e0e5657f8fd51dd5b
mdj2875 <mdj2875@gmail.com>
2025-12-15 10:49:52 -0500

    Update smoothie_app.py
    add select on

--- smoothie_app.py
+++ smoothie_app.py
@@ -16,7 +16,9 @@ st.write(
 name_on_order = st.text_input("Name On Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'),col('Search_On'))
+st.dataframe(data=my_dataframe,use_container_width=true)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to five ingredients:",
