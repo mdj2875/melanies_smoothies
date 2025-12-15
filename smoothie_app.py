d6717ddbe17a55c1330b81d7adec59eaff3e2ab1
mdj2875 <mdj2875@gmail.com>
2025-12-15 10:35:33 -0500

    Update smoothie_app.py

--- smoothie_app.py
+++ smoothie_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
-sf_df = st.dataframe(data=st.text(smoothiefroot_response.json()),use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
